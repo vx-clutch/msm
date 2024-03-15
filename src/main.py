25d14988513f1931256063af8b1373a4631fa459
vx_clutch <owestness@gmail.com>
2024-03-14 21:41:36 -0400

    save

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 # Imports
 import os
 import subprocess as sub
-import toml
+import toml as t
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.panel import Panel
 
 console = Console()
+config_path='../msm.toml'
+
+if os.path.exists(config_path):
+    if os.path.getsize(config_path) == 0:
+        subprocess.run(["./toml.sh"], shell=True)
 
 with open('../msm.toml', 'r') as f:
-    config = toml.load(f)
+    config = t.load(f)
 
 class Server:
     def __init__(self, name, path, modLoader, mods):
@@ -17,3 +22,4 @@ class Server:
         self.path = path
         self.modLoader = modLoader
         self.mods = mods
+print(config)
